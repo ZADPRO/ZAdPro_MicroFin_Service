af411bc8992fe7900375286b35a50e1a55cff7c7
vijayloganathan <vijayloganathan2002@gmail.com>
2025-07-12 13:53:43 +0530

    12/07/2025 Admin Loan Bug Fix

--- src/api/adminLoanCreation/query.ts
+++ src/api/adminLoanCreation/query.ts
@@ -764,7 +764,7 @@ export const updateBankFundQuery = `INSERT INTO
      "refFundType", 
      "refFundTypeId",
      "createdAt",
-     "createdBy",
+     "createdBy"
       )
  VALUES
    ($1, $2, $3, $4, $5, $6, $7, $8,$9)
